88485b930eb2bed44079c78933f6700da764680e
James Bole Pan <jamespan1118@hotmail.com>
2023-10-05 23:40:19 -0400

    was able to receive 110/100; good job

--- code_hw1_shortest/scatch_book.jl
+++ code_hw1_shortest/scatch_book.jl
@@ -1,15 +1,40 @@
-# Initialize sum
-global sum_val = 0
+using Distributed
 
-# Record the time taken to compute the sum
-elapsed_time = @elapsed begin
-    for i in 1:100000000
-        global sum_val += i
+# Function to calculate sum of first n integers
+function calculate_sum(n)
+    return sum(1:n)
+end
+
+# Add worker processes for parallel computing
+addprocs(4)
+
+@everywhere begin
+    # The function needs to be defined on all processes
+    function calculate_sum(n)
+        return sum(1:n)
     end
 end
 
-println("The time difference without distributed: ", elapsed_time)
-println("The sum is: ", sum_val)
+# Without parallel computing
+function serial_sum(n_values)
+    return [calculate_sum(n) for n in n_values]
+end
 
-# generate a movie from the png files
-run(`ffmpeg -framerate 10 -i movie/random_search_%d.png -c:v libx264 -r 30 -pix_fmt yuv420p movie/random_search.mp4`)
+# With parallel computing
+function parallel_sum(n_values)
+    return pmap(calculate_sum, n_values)
+end
+
+# List of n values
+n_values = [10^6, 10^7, 10^8, 10^9,10^6, 10^7, 10^8, 10^9,10^6, 10^7, 10^8, 10^9,10^6, 10^7, 10^8, 10^9]
+
+# Time serial execution
+println("Serial execution:")
+@time serial_results = serial_sum(n_values)
+
+# Time parallel execution
+println("\nParallel execution:")
+@time parallel_results = parallel_sum(n_values)
+
+# Check if results are the same
+println("\nAre the serial and parallel results the same? ", all(serial_results .== parallel_results))
